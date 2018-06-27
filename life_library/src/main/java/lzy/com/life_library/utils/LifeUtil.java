a5102cb932b30c678057353517f7ad886afb5f23
wangdanlizhiyun <742971453@qq.com>
2018-06-27 18:04:45 +0800

    add clearOtherAndStartActivity

--- life_library/src/main/java/lzy/com/life_library/utils/LifeUtil.java
+++ life_library/src/main/java/lzy/com/life_library/utils/LifeUtil.java
@@ -17,6 +17,7 @@ import android.util.LruCache;
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -36,40 +37,44 @@ import lzy.com.life_library.listener.ResultListener;
 import lzy.com.life_library.listener.ResultOkListener;
 
 public final class LifeUtil {
-    public static ResultListenerBuilder resultOk(ResultOkListener resultOkListener){
+    public static ResultListenerBuilder resultOk(ResultOkListener resultOkListener) {
         ResultListenerBuilder resultListenerBuilder = new ResultListenerBuilder();
         resultListenerBuilder.resultOk(resultOkListener);
         return resultListenerBuilder;
     }
 
     protected static void startActivityForResult(Intent intent, ResultListener resultListener) {
-        startActivityForResult(getActivity(),intent,resultListener);
+        startActivityForResult(getActivity(), intent, resultListener);
     }
 
     private static void startActivityForResult(@NonNull Activity activity, Intent intent, ResultListener resultListener) {
         LifeUtil.assertMainThread();
-        if (activity != null){
+        if (activity != null) {
             getFragment(activity).startActivityForResult(intent, resultListener);
         }
     }
+
     public static class ResultListenerBuilder {
         ResultOkListener resultOkListener;
         ResultCancelListener resultCancelListener;
         ResultFirstUserListener resultFirstUserListener;
-        public ResultListenerBuilder resultOk(ResultOkListener resultOkListener){
+
+        public ResultListenerBuilder resultOk(ResultOkListener resultOkListener) {
             this.resultOkListener = resultOkListener;
             return this;
         }
-        public ResultListenerBuilder resultCancel(ResultCancelListener resultCancelListener){
+
+        public ResultListenerBuilder resultCancel(ResultCancelListener resultCancelListener) {
             this.resultCancelListener = resultCancelListener;
             return this;
         }
-        public ResultListenerBuilder resultFirstUser(ResultFirstUserListener resultFirstUserListener){
+
+        public ResultListenerBuilder resultFirstUser(ResultFirstUserListener resultFirstUserListener) {
             this.resultFirstUserListener = resultFirstUserListener;
             return this;
         }
 
-        private ResultListener build(){
+        private ResultListener build() {
             ResultListener resultListener = new ResultListener();
             resultListener.setResultOkListener(resultOkListener);
             resultListener.setResultCancelListener(resultCancelListener);
@@ -77,33 +82,35 @@ public final class LifeUtil {
             return resultListener;
         }
 
-        public void startActivityForResult(Intent intent){
-            LifeUtil.startActivityForResult(intent,build());
+        public void startActivityForResult(Intent intent) {
+            LifeUtil.startActivityForResult(intent, build());
         }
     }
-    static LruCache<String,PermissionRequestBuilder> sPermissionRequestBuilderLruCache;
+
+    static LruCache<String, PermissionRequestBuilder> sPermissionRequestBuilderLruCache;
+
     public static PermissionRequestBuilder permission(String... permissions) {
-        if (sPermissionRequestBuilderLruCache == null){
+        if (sPermissionRequestBuilderLruCache == null) {
             sPermissionRequestBuilderLruCache = new LruCache<>(1);
         }
         PermissionRequestBuilder permissionRequestBuilder = sPermissionRequestBuilderLruCache.get("permissionRequestBuilderLruCache");
-        if (permissionRequestBuilder == null){
+        if (permissionRequestBuilder == null) {
             permissionRequestBuilder = new PermissionRequestBuilder();
-            sPermissionRequestBuilderLruCache.put("permissionRequestBuilderLruCache",permissionRequestBuilder);
+            sPermissionRequestBuilderLruCache.put("permissionRequestBuilderLruCache", permissionRequestBuilder);
         }
         return permissionRequestBuilder.permission(permissions);
     }
 
     private static void requestPermission(@NonNull final Activity activity, final PermissionRequest permissionRequest) {
         if (activity != null) {
-            if (isOnBackgroundThread()){
+            if (isOnBackgroundThread()) {
                 getMainHandler().post(new Runnable() {
                     @Override
                     public void run() {
                         getFragment(activity).doRequestPermissions(activity, permissionRequest);
                     }
                 });
-            }else {
+            } else {
                 getFragment(activity).doRequestPermissions(activity, permissionRequest);
             }
         }
@@ -112,7 +119,6 @@ public final class LifeUtil {
     public static void requestPermission(PermissionRequest permissionRequest) {
         requestPermission(getActivity(), permissionRequest);
     }
-
 
 
     public static boolean isNeedCheck(Context context) {
@@ -136,13 +142,16 @@ public final class LifeUtil {
     private static int mActivityCounts = 0;
     private static int mActivityCanSeeCounts = 0;
     static AtomicBoolean mIsAppInited = new AtomicBoolean(false);
+    private static List<WeakReference<Activity>> mActivities = new ArrayList<>();
+
     public static void init(Application application) {
-        if (mIsAppInited.compareAndSet(false,true)){
+        if (mIsAppInited.compareAndSet(false, true)) {
             application.registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacksAdapter() {
                 @Override
                 public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
                     super.onActivityCreated(activity, savedInstanceState);
-                    if (mActivityCounts == 0){
+                    addActivity(activity);
+                    if (mActivityCounts == 0) {
                         mWeakReferenceActivity = new WeakReference<Activity>(activity);
                     }
                     mActivityCounts++;
@@ -153,8 +162,8 @@ public final class LifeUtil {
                     super.onActivityStarted(activity);
                     mWeakReferenceActivity = new WeakReference<Activity>(activity);
                     mActivityCanSeeCounts++;
-                    if (mActivityCanSeeCounts == 1){
-                        if (mAppFourgroundOrBackgroundChangeListener != null){
+                    if (mActivityCanSeeCounts == 1) {
+                        if (mAppFourgroundOrBackgroundChangeListener != null) {
                             mAppFourgroundOrBackgroundChangeListener.change(false);
                         }
                         getMainHandler().removeMessages(WHAT_GotoBackgroundSomeTime);
@@ -165,17 +174,17 @@ public final class LifeUtil {
                 public void onActivityStopped(Activity activity) {
                     super.onActivityStopped(activity);
                     mActivityCanSeeCounts--;
-                    if (mActivityCanSeeCounts == 0){
-                        if (mAppFourgroundOrBackgroundChangeListener != null){
+                    if (mActivityCanSeeCounts == 0) {
+                        if (mAppFourgroundOrBackgroundChangeListener != null) {
                             mAppFourgroundOrBackgroundChangeListener.change(true);
                         }
-                        Iterator<Map.Entry<Integer,AppGotoBackgroundSomeTimeListener>> entryIterator = mAppGotoBackgroundSomeTimeListeners.entrySet().iterator();
-                        while (entryIterator.hasNext()){
+                        Iterator<Map.Entry<Integer, AppGotoBackgroundSomeTimeListener>> entryIterator = mAppGotoBackgroundSomeTimeListeners.entrySet().iterator();
+                        while (entryIterator.hasNext()) {
                             AppGotoBackgroundSomeTimeListener listener = entryIterator.next().getValue();
                             Message message = getMainHandler().obtainMessage();
                             message.what = WHAT_GotoBackgroundSomeTime;
                             message.arg1 = listener.hashCode();
-                            getMainHandler().sendMessageDelayed(message,listener.delayTime());
+                            getMainHandler().sendMessageDelayed(message, listener.delayTime());
                         }
                     }
                 }
@@ -183,63 +192,105 @@ public final class LifeUtil {
                 @Override
                 public void onActivityDestroyed(Activity activity) {
                     super.onActivityDestroyed(activity);
+                    removeActivity(activity);
                     InputMethodManagerUtil.fixInputMethodManagerLeak(activity);
                     mActivityCounts--;
                 }
             });
         }
     }
+    private synchronized static void addActivity(Activity activity){
+        mActivities.add(new WeakReference<Activity>(activity));
+    }
+    private synchronized static void removeActivity(Activity activity){
+        for (WeakReference<Activity> wr : mActivities
+                ) {
+            if (wr != null && wr.get() != null) {
+                Activity a = wr.get();
+                if (a.hashCode() == activity.hashCode()) {
+                    mActivities.remove(wr);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void clearOtherAndStartActivity(Context context,Class clazz){
+        finishAllActivities();
+        context.startActivity(new Intent(context,clazz));
+    }
+
+    public synchronized static void finishAllActivities() {
+        for (int i = mActivities.size() - 1; i >= 0; i--) {
+            WeakReference<Activity> wr = mActivities.get(i);
+            if (wr != null && wr.get() != null) {
+                Activity a = wr.get();
+                if (a != null && !a.isFinishing()) {
+                    a.finish();
+                }
+            }
+        }
+        mActivities.clear();
+    }
+
     public static void init(Activity activity) {
         init(activity.getApplication());
     }
+
     private static AppFourgroundOrBackgroundChangeListener mAppFourgroundOrBackgroundChangeListener;
 
     public static void setAppFourgroundOrBackgroundChangeListener(AppFourgroundOrBackgroundChangeListener mAppFourgroundOrBackgroundChangeListener) {
         LifeUtil.mAppFourgroundOrBackgroundChangeListener = mAppFourgroundOrBackgroundChangeListener;
     }
-    static ConcurrentHashMap<Integer,AppGotoBackgroundSomeTimeListener> mAppGotoBackgroundSomeTimeListeners = new ConcurrentHashMap<>();
+
+    static ConcurrentHashMap<Integer, AppGotoBackgroundSomeTimeListener> mAppGotoBackgroundSomeTimeListeners = new ConcurrentHashMap<>();
     private static final int WHAT_GotoBackgroundSomeTime = 1;
-    public static void addAppGotoBackgroundSomeTimeListener(AppGotoBackgroundSomeTimeListener listener){
-        mAppGotoBackgroundSomeTimeListeners.put(listener.hashCode(),listener);
+
+    public static void addAppGotoBackgroundSomeTimeListener(AppGotoBackgroundSomeTimeListener listener) {
+        mAppGotoBackgroundSomeTimeListeners.put(listener.hashCode(), listener);
     }
-    public static void releaseAppGotoBackgroundSomeTimeListener(AppGotoBackgroundSomeTimeListener listener){
+
+    public static void releaseAppGotoBackgroundSomeTimeListener(AppGotoBackgroundSomeTimeListener listener) {
         mAppGotoBackgroundSomeTimeListeners.remove(listener.hashCode());
     }
 
-    public static void addLifeCycle(final Activity activity,final LifeCycleListener lifeCycleListener){
-        if (isOnBackgroundThread()){
+    public static void addLifeCycle(final Activity activity, final LifeCycleListener lifeCycleListener) {
+        if (isOnBackgroundThread()) {
             getMainHandler().post(new Runnable() {
                 @Override
                 public void run() {
                     getFragment(activity).addLifeCycleListener(lifeCycleListener);
                 }
             });
-        }else {
+        } else {
             getFragment(activity).addLifeCycleListener(lifeCycleListener);
         }
     }
-    public static void addLifeCycle(final Fragment fragment, final LifeCycleListener lifeCycleListener){
-        if (isOnBackgroundThread()){
+
+    public static void addLifeCycle(final Fragment fragment, final LifeCycleListener lifeCycleListener) {
+        if (isOnBackgroundThread()) {
             getMainHandler().post(new Runnable() {
                 @Override
                 public void run() {
                     getFragment(fragment).addLifeCycleListener(lifeCycleListener);
                 }
             });
-        }else {
+        } else {
             getFragment(fragment).addLifeCycleListener(lifeCycleListener);
         }
     }
+
     static final Handler mMainHandler;
+
     static {
-        mMainHandler = new Handler(Looper.getMainLooper()){
+        mMainHandler = new Handler(Looper.getMainLooper()) {
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
-                switch (msg.what){
+                switch (msg.what) {
                     case WHAT_GotoBackgroundSomeTime:
                         AppGotoBackgroundSomeTimeListener appGotoBackgroundSomeTimeListener = mAppGotoBackgroundSomeTimeListeners.get(msg.arg1);
-                        if (appGotoBackgroundSomeTimeListener != null){
+                        if (appGotoBackgroundSomeTimeListener != null) {
                             appGotoBackgroundSomeTimeListener.gotoBackground();
                         }
                         break;
@@ -247,7 +298,8 @@ public final class LifeUtil {
             }
         };
     }
-    private static Handler getMainHandler(){
+
+    private static Handler getMainHandler() {
         return mMainHandler;
     }
 
@@ -264,6 +316,7 @@ public final class LifeUtil {
         }
         return fragment;
     }
+
     public static EmptyFragment getFragment(Fragment fragment) {
         EmptyFragment emptyFragment = findFragment(fragment);
         if (emptyFragment == null) {
@@ -281,35 +334,37 @@ public final class LifeUtil {
     private static EmptyFragment findFragment(Activity activity) {
         return (EmptyFragment) activity.getFragmentManager().findFragmentByTag(TAG);
     }
+
     private static EmptyFragment findFragment(Fragment fragment) {
         return (EmptyFragment) fragment.getFragmentManager().findFragmentByTag(TAG);
     }
 
     public static Activity getActivity() {
-        if (mWeakReferenceActivity != null){
+        if (mWeakReferenceActivity != null) {
             return mWeakReferenceActivity.get();
         }
         return null;
     }
 
 
-  public static void assertMainThread() {
-    if (!isOnMainThread()) {
-      throw new IllegalArgumentException("You must call this method on the main thread");
+    public static void assertMainThread() {
+        if (!isOnMainThread()) {
+            throw new IllegalArgumentException("You must call this method on the main thread");
+        }
     }
-  }
-  public static void assertBackgroundThread() {
-    if (!isOnBackgroundThread()) {
-      throw new IllegalArgumentException("You must call this method on a background thread");
+
+    public static void assertBackgroundThread() {
+        if (!isOnBackgroundThread()) {
+            throw new IllegalArgumentException("You must call this method on a background thread");
+        }
     }
-  }
 
-  public static boolean isOnMainThread() {
-    return Looper.myLooper() == Looper.getMainLooper();
-  }
+    public static boolean isOnMainThread() {
+        return Looper.myLooper() == Looper.getMainLooper();
+    }
 
-  public static boolean isOnBackgroundThread() {
-    return !isOnMainThread();
-  }
+    public static boolean isOnBackgroundThread() {
+        return !isOnMainThread();
+    }
 
 }
